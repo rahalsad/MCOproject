dd30a495f75939a57838e5274e4ed9e1c39ec5da
rahalsad <sadiqi.fatima.ezzahra@gmail.com>
2019-04-24 08:20:13 +0000

    version3.5

--- src/main/java/com/example/demo/entities/Fiche.java
+++ src/main/java/com/example/demo/entities/Fiche.java
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 package com.example.demo.entities;
 
 import java.io.Serializable;
@@ -106,122 +106,9 @@ public class Fiche implements Serializable  {
 	public void setDateModif(Date dateModif) {
 		this.dateModif = dateModif;
 	}
-=======
-package com.example.demo.entities;
-
-import java.io.Serializable;
-
-import java.util.Date;
-
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.Temporal;
-import javax.persistence.TemporalType;
 
 
-import org.springframework.format.annotation.DateTimeFormat;
 
-@Entity
-public class Fiche implements Serializable  {
-	/**
-	 * 
-	 */
-	
-	@Id @GeneratedValue 
-	private Long id;
-	private String iniName;
-	private String iniPrenom;
-	private String NomDirection; 
-	private String NomDepart;
-	private String NomApplication;
-	private String intituleMaintenance;
-	 @DateTimeFormat(pattern = "dd/MM/yyyy")
-	  // This is for bind Date with @ModelAttribute
-	    @Temporal(TemporalType.DATE)
-	    @Column(name = "inc_date")
-	  
-	 	private Date dateEnvoi;
-	 	private Date dateModif;
-	 	
-	 	public Fiche() {
-			super();
-			}
-			// TODO Auto-generated constructor stub
-	 	
-	 	public Fiche(String iniName, String iniPrenom, String nomDirection, String nomDepart, String nomApplication,
-				String intituleMaintenance, Date dateEnvoi, Date dateModif) {
-			super();
-			this.iniName = iniName;
-			this.iniPrenom = iniPrenom;
-			NomDirection = nomDirection;
-			NomDepart = nomDepart;
-			NomApplication = nomApplication;
-			this.intituleMaintenance = intituleMaintenance;
-			this.dateEnvoi = dateEnvoi;
-			this.dateModif = dateModif;
-		}
-	 	
-	 	
-	 	public Long getId() {
-		return id;
-	}
-	public void setId(Long id) {
-		this.id = id;
-	}
-	public String getIniName() {
-		return iniName;
-	}
-	public void setIniName(String iniName) {
-		this.iniName = iniName;
-	}
-	public String getIniPrenom() {
-		return iniPrenom;
-	}
-	public void setIniPrenom(String iniPrenom) {
-		this.iniPrenom = iniPrenom;
-	}
-	public String getNomDirection() {
-		return NomDirection;
-	}
-	public void setNomDirection(String nomDirection) {
-		NomDirection = nomDirection;
-	}
-	public String getNomDepart() {
-		return NomDepart;
-	}
-	public void setNomDepart(String nomDepart) {
-		NomDepart = nomDepart;
-	}
-	public String getNomApplication() {
-		return NomApplication;
-	}
-	public void setNomApplication(String nomApplication) {
-		NomApplication = nomApplication;
-	}
-	public String getIntituleMaintenance() {
-		return intituleMaintenance;
-	}
-	public void setIntituleMaintenance(String intituleMaintenance) {
-		this.intituleMaintenance = intituleMaintenance;
-	}
-	public Date getDateEnvoi() {
-		return dateEnvoi;
-	}
-	public void setDateEnvoi(Date dateEnvoi) {
-		this.dateEnvoi = dateEnvoi;
-	}
-	public Date getDateModif() {
-		return dateModif;
-	}
-	public void setDateModif(Date dateModif) {
-		this.dateModif = dateModif;
-	}
-		
-		
 
-		
-	 	
->>>>>>> branch 'master' of https://github.com/rahalsad/MCOproject
+
 }
--- src/main/java/com/example/demo/web/FicheController.java
+++ src/main/java/com/example/demo/web/FicheController.java
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 package com.example.demo.web;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -34,52 +34,3 @@ public String saveFiche(Model model ,@Valid Fiche fiche,
 }
 
 }
-=======
-package com.example.demo.web;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import javax.validation.Valid;
-
-
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-
-
-import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.PathVariable;
-
-import org.springframework.web.bind.annotation.RequestMethod;
-
-import com.example.demo.dao.FicheRepository;
-import com.example.demo.entities.Fiche;
-
-
-@Controller
-
-public class FicheController {
-	@Autowired
-	private FicheRepository FicheRepository; 
-	@RequestMapping(value="/fiche",method=RequestMethod.GET)
-	public String fiche(Model model ) {
-		model.addAttribute("Fiche",new Fiche());
-		return "fiche"; //LE NOM DE LA VUE.HTML
-	
-	}
-	
-
-@RequestMapping(value="/saveFiche",method=RequestMethod.POST)
-public String saveFiche(Model model ,@Valid Fiche Fiche,
-		BindingResult bindingResult) {
-	if (bindingResult.hasErrors())
-		return "fiche";
-	FicheRepository.save(Fiche);
-	return "confirmDemande"; //LE NOM DE LA VUE.HTML
-
-}
-	
-}
->>>>>>> branch 'master' of https://github.com/rahalsad/MCOproject
